9da1cf5884c35c03a35b2c13d50dd0312a8817b6
ShaguftaS <shaguftha.96@gmail.com>
2024-07-08 20:24:14 +0530

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.*;
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i =  " + i);
+class Test
+{
+    // Pushing element on the top of the stack
+    static void stack_push(Stack<Integer> stack)
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            stack.push(i);
         }
+    }
+
+    // Popping element from the top of the stack
+    static void stack_pop(Stack<Integer> stack)
+    {
+        System.out.println("Pop Operation:");
+
+        for(int i = 0; i < 5; i++)
+        {
+            Integer y = (Integer) stack.pop();
+            System.out.println(y);
+        }
+    }
+
+    // Displaying element on the top of the stack
+    static void stack_peek(Stack<Integer> stack)
+    {
+        Integer element = (Integer) stack.peek();
+        System.out.println("Element on stack top: " + element);
+    }
+
+    // Searching element in the stack
+    static void stack_search(Stack<Integer> stack, int element)
+    {
+        Integer pos = (Integer) stack.search(element);
+
+        if(pos == -1)
+            System.out.println("Element not found");
+        else
+            System.out.println("Element is found at position: " + pos);
+    }
+
+
+    public static void main (String[] args)
+    {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        stack_push(stack);
+        stack_pop(stack);
+        stack_push(stack);
+        stack_peek(stack);
+        stack_search(stack, 2);
+        stack_search(stack, 6);
     }
 }
