c16a144dda3f908617e07498b3f7e7e3f2516577
ShaguftaS <shaguftha.96@gmail.com>
2024-07-11 00:11:27 +0530

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import java.io.*;
 import java.util.*;
 
 class Test
 {
-    // Pushing element on the top of the stack
+
     static void stack_push(Stack<Integer> stack)
     {
         for(int i = 0; i < 5; i++)
@@ -12,29 +11,29 @@ class Test
         }
     }
 
-    // Popping element from the top of the stack
+
     static void stack_pop(Stack<Integer> stack)
     {
         System.out.println("Pop Operation:");
 
         for(int i = 0; i < 5; i++)
         {
-            Integer y = (Integer) stack.pop();
+            Integer y = stack.pop();
             System.out.println(y);
         }
     }
 
-    // Displaying element on the top of the stack
+
     static void stack_peek(Stack<Integer> stack)
     {
-        Integer element = (Integer) stack.peek();
+        Integer element = stack.peek();
         System.out.println("Element on stack top: " + element);
     }
 
-    // Searching element in the stack
+
     static void stack_search(Stack<Integer> stack, int element)
     {
-        Integer pos = (Integer) stack.search(element);
+        int pos = stack.search(element);
 
         if(pos == -1)
             System.out.println("Element not found");
@@ -45,7 +44,7 @@ class Test
 
     public static void main (String[] args)
     {
-        Stack<Integer> stack = new Stack<Integer>();
+        Stack<Integer> stack = new Stack<>();
 
         stack_push(stack);
         stack_pop(stack);
