cab62e59edd68966d32d8efd2217daf3f3575154
ShaguftaS <shaguftha.96@gmail.com>
2024-07-11 00:05:43 +0530

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,18 @@
+public class Example {
+
+
+
+    void findNemo(String[] array) {
+        for (String s : array) {
+            if (s.equals("nemo")) {
+                System.out.println("Found Nemo!");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Example example = new Example();
+        String[] arrayToSearch = {"nemo", "dory", "marlin"};
+        example.findNemo(arrayToSearch);
+    }
+}
