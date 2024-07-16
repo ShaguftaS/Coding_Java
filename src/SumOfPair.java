6f2053b563a1e69de7961c6c34234464bafdb46d
ShaguftaS <shaguftha.96@gmail.com>
2024-07-17 00:49:45 +0530

    Initial commit

--- src/SumOfPair.java
+++ src/SumOfPair.java
@@ -5,7 +5,7 @@ public class SumOfPair {
                 if(A[i] + A[j] == x) return true;
             }
         }
-        return false;
+        return false ;
     }
     public static void main( String[] args ){
         int[] A ;
