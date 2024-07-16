030abc57708ee592272f8a4d82ad99e4fd0db528
ShaguftaS <shaguftha.96@gmail.com>
2024-07-17 00:50:36 +0530

    Initial commit

--- src/SumOfPair.java
+++ src/SumOfPair.java
@@ -1,5 +1,5 @@
 public class SumOfPair {
-    static boolean check( int[] A, int size, int x){
+    static boolean check ( int[] A, int size, int x ){
         for( int i =0; i < (size -1); i++ ){
             for( int j = (i+1); j < size; j++){
                 if(A[i] + A[j] == x) return true;
