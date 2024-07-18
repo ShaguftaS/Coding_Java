5644e1b302830440e035f8ba20ad603218328b2c
ShaguftaS <shaguftha.96@gmail.com>
2024-07-18 23:17:40 +0530

    Initial commit

---
+++ src/logAllPairsOfArray.java
@@ -0,0 +1,14 @@
+public class logAllPairsOfArray {
+    public static void main(String[] args) {
+        int[] boxes = {1, 2, 3, 4, 5};
+        logAllPairs(boxes);
+    }
+
+    static void logAllPairs(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.println("i = " + i + ", j = " + j);
+            }
+        }
+    }
+}
