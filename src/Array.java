7da30be57b945a37f9a4dd1267fea41296fb1d1a
ShaguftaS <shaguftha.96@gmail.com>
2024-07-21 23:52:23 +0530

    Initial commit

--- src/Array.java
+++ src/Array.java
@@ -18,12 +18,22 @@ public class Array {
         if (this.length == 0) {
             throw new IllegalStateException("Cannot pop from an empty array.");
         }
-
         String lastItem = this.data[this.length - 1];
         this.data[this.length - 1] = null;  // Clearing the reference to help garbage collection
         this.length--;
 
         return lastItem;
+    }
+    public void delete(int index) {
+        if (index < 0 || index >= this.length) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds.");
+        }
+
+        for (int i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        this.data[this.length - 1] = null;  // Clearing the last element
+        this.length--;
     }
 
     public static void main ( String[] args ) {
@@ -31,8 +41,9 @@ public class Array {
         arr.push("hi");
         arr.push("you");
         arr.push("!");
-        arr.pop();
-        arr.pop();
+        arr.delete(1);
+        //arr.pop();
+        //arr.pop();
         System.out.println(arr.get(0));
 
     }
