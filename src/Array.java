ff184625dcaddcc193f2e03e60f1f6aa8ceff3a8
ShaguftaS <shaguftha.96@gmail.com>
2024-07-21 22:52:18 +0530

    Initial commit

---
+++ src/Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    public Array(){
+        int length =0;
+        String[] data;
+    }
+    public static void main ( String[] args ) {
+        Array arr = new Array();
+
+    }
+}
