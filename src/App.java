aacdc7823fd81df80e85dfe9e84e0a9e1a39714b
ShaguftaS <shaguftha.96@gmail.com>
2024-07-26 23:50:50 +0530

    Initial commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args){
+    public static void main( String[] args){
         Queue myQueue = new Queue(5);
         myQueue.insert(100);
         myQueue.insert(1000);
