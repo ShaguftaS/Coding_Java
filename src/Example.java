ff763e4a5d44a182526fded085ba7fef762cc0ba
ShaguftaS <shaguftha.96@gmail.com>
2024-07-31 15:26:14 +0530

    Initial commit

--- src/Example.java
+++ src/Example.java
@@ -10,7 +10,7 @@ public class Example {
 
     public static void main ( String[] args ) {
         Example example = new Example();
-        String[] arrayToSearch = {" nemo", " dory", " marlin"};
+        String[] arrayToSearch = {"nemo", " dory", " marlin"};
         example.findNemo(arrayToSearch);
     }
 }
