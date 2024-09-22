11a48236bc2935b4f9bf652abcc2d4583a29c867
ShaguftaS <shaguftha.96@gmail.com>
2024-09-22 23:28:05 +0530

    Initial commit

--- src/AppLL.java
+++ src/AppLL.java
@@ -3,7 +3,14 @@ public class AppLL {
         Node nodeA = new Node();
         nodeA.data = 4;
 
+        Node nodeB = new Node();
+        nodeB.data = 4;
 
+        Node nodeC = new Node();
+        nodeC.data = 4;
+
+        Node nodeD = new Node();
+        nodeD.data = 4;
 
     }
 }
